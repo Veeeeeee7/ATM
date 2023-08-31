cdcc40de47648c2176a687f49b2749592c2f8dd5
Victor <victorli1076@gmail.com>
2023-08-31 10:24:39 -0700

    Created checkBalance depositMoney and withdrawMoney

--- ATM.java
+++ ATM.java
@@ -27,4 +27,40 @@ public class ATM {
         }
     }
 
+    public double checkBalance(String email) throws Exception {
+        if (accounts.containsKey(email)) {
+            return accounts.get(email);
+        } else {
+            throw new Exception("no account is found");
+        }
+    }
+
+    public void depositMoney(String email, double deposit) throws Exception {
+        if (accounts.containsKey(email)) {
+            if (deposit > 0) {
+                accounts.put(email, accounts.get(email) + deposit);
+            } else {
+                throw new Exception("cant deposit debt *******");
+            }
+        } else {
+            throw new Exception("broke AF");
+        }
+    }
+
+    public void withdrawMoney(String email, double amount) throws Exception {
+        if (accounts.containsKey(email)) {
+            if (amount > 0) {
+                if (accounts.get(email) >= amount) {
+                    accounts.put(email, accounts.get(email) - amount);
+                } else {
+                    throw new Exception("broke AFFFFF");
+                }
+            } else {
+                throw new Exception("cant withdraw debt *******");
+            }
+        } else {
+            throw new Exception("broke AF");
+        }
+    }
+
 }
--- tester.java
+++ tester.java
@@ -2,7 +2,9 @@ public class tester {
     public static void main(String[] args) throws Exception {
         ATM bank = new ATM();
         bank.openAccount("hi@gmail.com", 0);
-
+        bank.depositMoney("hi@gmail.com", 100);
+        System.out.println("$$$" + bank.checkBalance("hi@gmail.com"));
+        bank.withdrawMoney("hi@gmail.com", 200);
         bank.closeAccount("hi@gmail.com");
     }
 }
